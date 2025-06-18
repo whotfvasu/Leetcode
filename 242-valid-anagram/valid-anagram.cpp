7d5366640228c227000c21c2ceff040100263fa2
Vasu Parashar <vasuparashar18@gmail.com>
2025-06-18 15:09:43 +0530

    Time: 3 ms (52.52%) | Memory: 9.9 MB (11.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        if(n!=m) return false;
-        map<char,int> mpp;
-        for(int i=0;i<n;i++){
-            mpp[s[i]]++;
+        if (s.length() != t.length()) {
+            return false;
         }
-        for(int i=0;i<n;i++){
-            if(mpp.find(t[i])!=mpp.end()){
-                mpp[t[i]]--;
-                if(mpp[t[i]]==0){
-                    mpp.erase(t[i]);
-                }
+
+        unordered_map<char, int> counter;
+
+        for (char ch : s) {
+            counter[ch] = counter[ch] + 1;
+        }
+
+        for (char ch : t) {
+            if (counter.find(ch) == counter.end() || counter[ch] == 0) {
+                return false;
             }
-            else return false;
+            counter[ch] = counter[ch] - 1;
         }
-        return true;
+
+        return true;        
     }
 };
