8dbab52d44440e9123982dca76d5c8ea53cde2ed
Vasu Parashar <vasuparashar18@gmail.com>
2025-06-19 23:18:39 +0530

    Time: 3 ms (18.45%) | Memory: 9.3 MB (62.02%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char,int> mpp;
+        int n = s.length();
+        for(char c: s){
+            mpp[c]++;
+        }
+        int sum = 0;
+        for(auto it: mpp){
+            sum+=it.second/2;
+        }
+        if(sum*2==n) return sum*2;
+        else return sum*2+1;
+    }
+};
