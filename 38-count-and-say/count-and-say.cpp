7d8ff7989d4c2645bcdc82e0a900e8df8ff4f387
Vasu Parashar <vasuparashar18@gmail.com>
2025-07-07 05:40:41 +0530

    Time: 3 ms (83.21%) | Memory: 10 MB (45.97%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    vector<pair<char,int>> helper1(string s){
-        vector<pair<char,int>> v;
-        int n = s.length();
-        int i=0;
-        while(i<n){
-            int cnt = 1;
-            while(i+1<n && s[i]==s[i+1]){
+    // vector<pair<char,int>> helper1(string s){
+    //     vector<pair<char,int>> v;
+    //     int n = s.length();
+    //     int i=0;
+    //     while(i<n){
+    //         int cnt = 1;
+    //         while(i+1<n && s[i]==s[i+1]){
+    //             i++;
+    //             cnt++;
+    //         }
+    //         v.push_back({s[i],cnt});
+    //         i++;
+    //     }
+    //     return v;
+    // }
+    // string helper2(vector<pair<char,int>> v){
+    //     string s = "";
+    //     int n = v.size();
+    //     for(auto it: v){
+    //         s+= to_string(it.second);
+    //         s+= it.first;
+    //     }
+    //     return s;
+    // }
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+
+        string prev = countAndSay(n - 1);
+        string res = "";
+        int i = 0;
+
+        while (i < prev.size()) {
+            int count = 1;
+            while (i + 1 < prev.size() && prev[i] == prev[i + 1]) {
                 i++;
-                cnt++;
+                count++;
             }
-            v.push_back({s[i],cnt});
+            res += to_string(count) + prev[i];
             i++;
         }
-        return v;
-    }
-    string helper2(vector<pair<char,int>> v){
-        string s = "";
-        int n = v.size();
-        for(auto it: v){
-            s+= to_string(it.second);
-            s+= it.first;
-        }
-        return s;
-    }
-    string countAndSay(int n) {
-        string s = "1";
-        unordered_map<int,int> mpp;
-        n--;
-        while(n--){
-            vector<pair<char,int>> v =helper1(s);
-            s = helper2(v);
-        }
-        return s;
+
+        return res;
     }
 };
