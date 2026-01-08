2e209e3214c95c3f69f58a14fecdc2cba870d8c7
Vasu Parashar <vasuparashar18@gmail.com>
2026-01-09 03:34:50 +0530

    Time: 3 ms (38.27%) | Memory: 13.8 MB (89.79%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& v1, vector<int>& v2) {
-        vector<int> result;
-        set<int> st1(v1.begin(),v1.end());
-        set<int> st2(v2.begin(),v2.end());
-        for( auto nums: st1){
-            if(st2.find(nums)!= st2.end()){
-                result.push_back(nums);
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // Always binary search in larger array
+        if (nums1.size() > nums2.size())
+            return intersection(nums2, nums1);
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> ans;
+        int i = 0;
+
+        while (i < nums1.size()) {
+            int val = nums1[i];
+
+            // skip duplicates in nums1
+            while (i < nums1.size() && nums1[i] == val)
+                i++;
+
+            // binary search in nums2
+            if (binary_search(nums2.begin(), nums2.end(), val)) {
+                ans.push_back(val);
             }
         }
-        return result;
+        return ans;
     }
 };
