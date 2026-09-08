0f133c06008eba70bba2be1804ee56ab3ea499e5
Vasu Parashar <vasuparashar18@gmail.com>
2026-09-08 22:19:08 +0530

    Time: 3 ms (91.84%) | Memory: 19.1 MB (55.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
+        vector<int> last(256, -1);
+
         int left = 0;
         int ans = 0;
 
-        unordered_map<char, int> mp;
-
-        for (int right = 0; right < n; right++) {
-            mp[s[right]]++;
-
-            while (mp.size() < right-left+1) {
-                mp[s[left]]--;
-
-                if (mp[s[left]] == 0)
-                    mp.erase(s[left]);
-
-                left++;
+        for (int right = 0; right < s.size(); right++) {
+            if (last[s[right]] >= left) {
+                left = last[s[right]] + 1;
             }
 
-            if(mp.size() == right-left+1){
-                ans = max(ans, right - left + 1);
-            }
+            last[s[right]] = right;
+
+            ans = max(ans, right - left + 1);
         }
 
         return ans;
-
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.length();
+//         int left = 0;
+//         int ans = 0;
+
+//         unordered_map<char, int> mp;
+
+//         for (int right = 0; right < n; right++) {
+//             mp[s[right]]++;
+
+//             while (mp.size() < right-left+1) {
+//                 mp[s[left]]--;
+
+//                 if (mp[s[left]] == 0)
+//                     mp.erase(s[left]);
+
+//                 left++;
+//             }
+
+//             if(mp.size() == right-left+1){
+//                 ans = max(ans, right - left + 1);
+//             }
+//         }
+
+//         return ans;
+
+//     }
+// };
