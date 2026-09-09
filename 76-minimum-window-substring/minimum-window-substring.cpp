255f9f0ebeb53188683545479b92444d538a6e83
Vasu Parashar <vasuparashar18@gmail.com>
2026-09-09 19:59:43 +0530

    Time: 195 ms (5.08%) | Memory: 12 MB (5.94%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    bool sahi(vector<int> &have, vector<int> &needed){
-        for(int i=0;i<256;i++){
-            if(have[i] < needed[i]) return false;
+    bool sahi(vector<int>& have, vector<int>& needed) {
+        for(int i = 0; i < 256; i++) {
+            if(have[i] < needed[i])
+                return false;
         }
         return true;
     }
-    string minWindow(string s, string t) {
-        vector<int> have(256); vector<int> needed(256);
-        for(int i=0;i<t.length();i++){
-            needed[t[i]]++;
-        }
-        int left = 0, res = INT_MAX, start = 0;
 
-        for(int right=0; right<s.length(); right++){
+    string minWindow(string s, string t) {
+        vector<int> have(256);
+        vector<int> needed(256);
+
+        for(char c : t) {
+            needed[c]++;
+        }
+
+        int left = 0;
+        int res = INT_MAX;
+        int start = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+
             have[s[right]]++;
-            while(sahi(have,needed)){
-                
-                int len = right-left+1;
-                if(res>len){
+
+            while(sahi(have, needed)) {
+
+                int len = right - left + 1;
+
+                if(len < res) {
                     res = len;
                     start = left;
                 }
+
                 have[s[left]]--;
                 left++;
             }
         }
+
         if(res == INT_MAX)
             return "";
-        return s.substr(start,res);
 
+        return s.substr(start, res);
     }
 };
