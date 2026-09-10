d200f65fc956e16b31c7187b76f7733b4c3b9972
Vasu Parashar <vasuparashar18@gmail.com>
2026-09-10 18:39:38 +0530

    Time: 14 ms (27.82%) | Memory: 10 MB (46.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     bool extra(vector<int> &have, vector<int> &needed){
-        for(int i=0;i<256;i++){
+        for(int i=0;i<26;i++){
             if(have[i]>needed[i]) return true;
         }
         return false;
     }
     bool check(vector<int> &have, vector<int> &needed){
-        for(int i=0;i<256;i++){
+        for(int i=0;i<26;i++){
             if(needed[i]!=have[i]) return false;
         }
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        vector<int> have(256);
-        vector<int> needed(256);
+        vector<int> have(26);
+        vector<int> needed(26);
 
         for(char c : s1) {
-            needed[c]++;
+            needed[c-'a']++;
         }
 
         int left = 0;
 
         for(int right=0;right<s2.size();right++){
-            have[s2[right]]++;
+            have[s2[right]-'a']++;
             while(extra(have,needed)){
-                have[s2[left]]--;
+                have[s2[left]-'a']--;
                 left++;
             }
             if(check(have,needed)==true) return true;
